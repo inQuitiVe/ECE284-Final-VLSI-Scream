0e23aa06214182e1733721c407d90b4c3c2b4a84
inquisitix <55553290+inQuitiVe@users.noreply.github.com>
2025-11-28 03:18:27 -0800

    Modify weight inputs and product calculations

--- hardware/src/mac_flex.v
+++ hardware/src/mac_flex.v
@@ -5,7 +5,7 @@ module mac (out, a, b, c, act_4b_mode);
     
     output signed [psum_bw-1:0] out;
     input signed  [bw-1:0]      a;  // activation
-    input signed  [bw-1:0]      b;  // weight
+    input signed  [bw-1:0]      b0, b1;  // weight
     input signed  [psum_bw-1:0] c;
     input                       act_4b_mode;   // 0 = 2-bit act, 1 = 4-bit act
 
@@ -18,13 +18,13 @@ module mac (out, a, b, c, act_4b_mode);
     assign a_hi_pad   = {1'b0, a[3:2]}; // force to be unsigned number
     assign a_lo_pad   = {1'b0, a[1:0]}; // force to be unsigned number
 
-    assign product_lo = $signed(a_lo_pad) * $signed(b);
-    assign product_hi = $signed(a_hi_pad) * $signed(b);
+    assign product_lo = $signed(a_lo_pad) * $signed(b0);
+    assign product_hi = $signed(a_hi_pad) * $signed(b1);
 
     assign product_lo_pad = {{2{product_lo[6]}}, product_lo[5:0]};
     assign product_hi_pad = {product_hi[5:0]   ,            2'b0};
 
-    assign product    = act_4b_mode ? $signed(product_lo_pad) + $signed(product_hi_pad) : $signed(product_lo_pad);
+    assign product    = act_4b_mode ? $signed(product_lo_pad) + $signed(product_hi_pad) : $signed(product_lo) + $signed(product_hi);
 
     assign psum       = $signed(product) + $signed(c);
     assign out        = psum;
