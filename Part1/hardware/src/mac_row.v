cbdd0179c02f8f84d13d03cf5df0247cbbae9e05
Chiang <b1chiang@ieng6-ece-17.ucsd.edu>
2025-11-28 16:05:21 -0800

    Project Delivery: First Commit

--- Part1/hardware/src/mac_row.v
+++ Part1/hardware/src/mac_row.v
@@ -14,25 +14,26 @@ module mac_row (clk, out_s, in_w, in_n, valid, inst_w, reset);
   input  [psum_bw*col-1:0] in_n;
 
   wire  [(col+1)*bw-1:0] temp;
-  wire  [(col+1)*2-1:0] inst_temp;
-
   assign temp[bw-1:0]   = in_w;
-  assign inst_temp[1:0] = inst_w;
+
+  wire  [(col+1)*2-1:0]  inst_bus;
+  assign inst_bus[1:0]   = inst_w;
 
   genvar i;
   generate
-    for (i=0; i < col ; i=i+1) begin : col_num
-        mac_tile #(.bw(bw), .psum_bw(psum_bw)) mac_tile_instance (
-          .clk(clk),
-          .reset(reset),
-          .in_w( temp[bw*(i+1)-1:bw*i]),
-          .out_e(temp[bw*(i+2)-1:bw*(i+1)]),
-          .inst_w(inst_temp[2*(i+1)-1:2*i]),
-          .inst_e(inst_temp[2*(i+2)-1:2*(i+1)]),
-          .in_n(in_n[psum_bw*(i+1)-1:psum_bw*i]),
-          .out_s(out_s[psum_bw*(i+1)-1:psum_bw*i]));
-        assign valid[i] = inst_temp[2*(i+1)+1];
+    for (i=1; i < col+1 ; i=i+1) begin : col_num
+      mac_tile #(.bw(bw), .psum_bw(psum_bw)) mac_tile_instance (
+        .clk(clk),
+        .reset(reset),
+        .in_w( temp[bw*i-1:bw*(i-1)]),
+        .out_e(temp[bw*(i+1)-1:bw*i]),
+        .inst_w(inst_bus[2*i-1 : 2*(i-1)]),
+        .inst_e(inst_bus[2*(i+1)-1 : 2*i]),
+        .in_n(in_n[psum_bw*i-1 : psum_bw*(i-1)]),
+        .out_s(out_s[psum_bw*i-1 : psum_bw*(i-1)]));
+
+      assign valid[i-1] = inst_bus[2*(i+1)-1];
     end
   endgenerate
-
+  
 endmodule
--- README.md
+++ README.md
@@ -1,18 +1,14 @@
-# ECE284-Project-Description-FA2025
-SIMD and Weight-Output reconfigurable 2D systolic array-based AI accelerator and mapping on Cyclone IV GX
-# TODO alpha
-1. clock gating in WS/ OS         0.5v (gary/vincent)
-2. BN - ReLU - MaxPool            (wait for ofifo)
-3. OS act tiling / weight tiling  (base)
-4. whole conv layer               (tbd)
-5. whole model                    (tbd)
-6. huffman/rlc                    (gary/vincent)
-7. prob2 + prob3                  v
+# README.md
+## Summary & Outline
+For project delivery.
+Part 1. Vanilla Version
+Part 2. 2b/4b reconfigurable SIMD Version
+Part 3. OS/WS reconfigurable Version
+Part 4. Fancy Alpha Version
 
-# SW source request
-1. BN fusion parameter
-2. BN output
-3. BN + ReLU output
-4. BN + ReLU + MaxPool output
-5. os activation reordering
-6. os tiling reordering
+## Structure
+### hardware
+    - src: verilog code (contains tb)
+    - golden: txt files that tb needs, and the ones with "viz" prefix are for human readibility
+### software
+    - ipynb: tb file that I generated. It has the correct format nad structure, but I used my own model that has only 90% validation accuracy.
