b953f50b6b8c986512203f54771f2ad3ae774c97
BingCheng <cbc20020515@gmail.com>
2025-11-30 04:31:28 -0800

    tb clear up first attempt

--- Part1/hardware/src/core_tb.v
+++ Part1/hardware/src/core_tb.v
@@ -15,43 +15,29 @@ parameter len_nij = 36;
 reg clk = 0;
 reg reset = 1;
 
+// High level instrucion & data for mac array.
+reg [1:0]  inst_w; 
+reg [bw*row-1:0] D_xmem;
 
-reg [1:0]  inst_w_q = 0; 
-reg [bw*row-1:0] D_xmem_q = 0;
+// X_MEM Ctrl(for activation.txt and weight.txt writing)
 reg CEN_xmem = 1;
 reg WEN_xmem = 1;
 reg [10:0] A_xmem = 0;
-reg CEN_xmem_q = 1;
-reg WEN_xmem_q = 1;
-reg [10:0] A_xmem_q = 0;
-reg CEN_pmem = 1;
-reg WEN_pmem = 1;
-reg [10:0] A_pmem = 0;
-reg CEN_pmem_q = 1;
-reg WEN_pmem_q = 1;
-reg [10:0] A_pmem_q = 0;
 
-reg acc_q = 0;
-reg acc = 0;
-
-
-reg [3:0] kij_q;
-
-reg [1:0]  inst_w; 
-reg [bw*row-1:0] D_xmem;
-reg [psum_bw*col-1:0] answer;
+// Readout (for output.txt verification. tb has no access to PSUM mem)
 reg readout_start;
 wire [psum_bw*col-1:0] readout;
 
+// Control Signal for SFU. I hope to get rid of this in the future versions.
+reg [3:0] kij_SFUctrl;
 
+
+// Misc 
 reg [8*30:1] stringvar;
 reg [8*50:1] w_file_name;
-wire ofifo_valid;
-wire [col*psum_bw-1:0] sfp_out;
-
+reg [psum_bw*col-1:0] answer;
 integer x_file, x_scan_file ; // file_handler
 integer w_file, w_scan_file ; // file_handler
-integer acc_file, acc_scan_file ; // file_handler
 integer out_file, out_scan_file ; // file_handler
 integer captured_data; 
 integer t, i, j, k, kij;
@@ -61,32 +47,24 @@ integer error;
 core #(.bw(bw), .psum_bw(psum_bw), .col(col), .row(row)) core_instance (
 	.clk(clk), 
   .reset(reset),
-  // high level instructions from TB
-  .inst_w(inst_w),
-  // L0 mem ctrls from TB
-  .CEN_xmem(CEN_xmem),
+  .inst_w(inst_w),     // high level instructions from TB
+  .CEN_xmem(CEN_xmem), // x_mem ctrls from TB
   .WEN_xmem(WEN_xmem),
   .A_xmem(A_xmem),
   .D_xmem(D_xmem), 
-  // PSUM mem ctrls from TB
-  // .CEN_pmem(),
-  // .WEN_pmem(),
-  // .A_pmem(),
-  // Output to TB
-  .kij(kij_q),
-  .readout_start(readout_start),
+  .kij(kij_SFUctrl),        // SFU control
+  .readout_start(readout_start), // Output to TB
   .readout(readout)
-	); 
+); 
 
 
 initial begin 
-
   inst_w   = 0; 
   D_xmem   = 0;
   CEN_xmem = 1;
   WEN_xmem = 1;
   A_xmem   = 0;
-  kij_q    = 0;
+  kij_SFUctrl    = 0;
   readout_start = 0;
 
   $dumpfile("core_tb.vcd");
@@ -115,11 +93,11 @@ initial begin
   /////////////////////////
 
   /////// Activation data writing to memory ///////
-  $display("===================== Activation.txt Writing Start ====================");
+  $display("## Activation.txt Writing to X_MEM Start ");
   for (t=0; t<len_nij; t=t+1) begin  
     #0.5 clk = 1'b0;  x_scan_file = $fscanf(x_file,"%32b", D_xmem); WEN_xmem = 0; CEN_xmem = 0; if (t>0) A_xmem = A_xmem + 1;
     #0.5 clk = 1'b1;   
-    $display("Writing to address %5d", A_xmem);
+    // $display("Writing to address %5d", A_xmem);
   end
   
   #0.5 clk = 1'b0;  WEN_xmem = 1;  CEN_xmem = 1; A_xmem = 0;
@@ -127,13 +105,12 @@ initial begin
 
   $fclose(x_file);
   /////////////////////////////////////////////////
-  $display("===================== Activation.txt Writing End ====================");
+  $display("## Activation.txt Writing to X_MEM End");
 
 
   for (kij=0; kij<9; kij=kij+1) begin  // kij loop
-
-
-    kij_q = kij;
+    kij_SFUctrl = kij;
+    $display("## kij=%1d", kij);
 
     case(kij)
      0: w_file_name = "golden/weight_itile0_otile0_kij0.txt";
@@ -172,60 +149,45 @@ initial begin
     #0.5 clk = 1'b0;   
     #0.5 clk = 1'b1;   
 
-    // 
+
 
 
 
     /////// Kernel data writing to memory ///////
+    $display("   weight.txt Writing to X_MEM Start ");
     A_xmem = 11'b10000000000;
     for (t=0; t<col; t=t+1) begin  
       #0.5 clk = 1'b0;  w_scan_file = $fscanf(w_file,"%32b", D_xmem); WEN_xmem = 0; CEN_xmem = 0; if (t>0) A_xmem = A_xmem + 1; 
       #0.5 clk = 1'b1;  
-      $display("Weight.txt %3d Writing to address %5d", kij, A_xmem);
     end
 
     #0.5 clk = 1'b0;  WEN_xmem = 1;  CEN_xmem = 1; A_xmem = 0;
     #0.5 clk = 1'b1; 
-
+    $display("   weight.txt Writing to X_MEM End");
     /////////////////////////////////////
 
-
     /////// Kernel data writing to L0 ///////
+    $display("   Weight Feeding to L0FIFO Start");
     A_xmem = 11'b10000000000;
     for (t=0; t<col; t=t+1) begin  
       #0.5 clk = 1'b0; WEN_xmem = 1; CEN_xmem = 0; inst_w = 2'b01; if (t>0) A_xmem = A_xmem + 1;
       #0.5 clk = 1'b1;  
-      $display("Kernel %3d Loading to L0FIFO", kij);
-    end
+    end  
     
-    /////////////////////////////////////
 
-
-
-    /////// Kernel loading to PEs ///////
-    // control signal generated in core.v
-    /////////////////////////////////////
-  
-
-
-    ////// provide some intermission to clear up the kernel loading ///
+    $display("   Weight Feeding to L0FIFO End.");
     #0.5 clk = 1'b0;  inst_w = 2'b00; WEN_xmem = 1; CEN_xmem = 1;
     #0.5 clk = 1'b1;  
-  
-    for (i=0; i<10 ; i=i+1) begin
-      #0.5 clk = 1'b0;
-      #0.5 clk = 1'b1;  
-    end
     /////////////////////////////////////
 
 
 
     /////// Activation data writing to L0 ///////
+    $display("   Activation Feeding to L0FIFO Start");
     A_xmem = 11'b00000000000;
     for (t=0; t<len_nij; t=t+1) begin  
       #0.5 clk = 1'b0; WEN_xmem = 1; CEN_xmem = 0; inst_w = 2'b10; if (t>0) A_xmem = A_xmem + 1;
       #0.5 clk = 1'b1;  
-      $display("Activation t=%2d Loading to L0FIFO", t);
     end
     /////////////////////////////////////
 
@@ -235,100 +197,62 @@ initial begin
     ////// provide some intermission to clear up the activation running ///
     #0.5 clk = 1'b0;  WEN_xmem = 1; CEN_xmem = 1; inst_w = 2'b00;
     #0.5 clk = 1'b1;  
-  
+    $display("   Activation Feeding End. Waiting for PSUM flowing\n");
     for (i=0; i<30 ; i=i+1) begin
       #0.5 clk = 1'b0;
       #0.5 clk = 1'b1;  
     end
     /////////////////////////////////////
     /////////////////////////////////////
-
-
-
-    //////// OFIFO READ ////////
-    // Ideally, OFIFO should be read while execution, but we have enough ofifo
-    // depth so we can fetch out after execution.
-    // ...
-    /////////////////////////////////////
-
-
   end  // end of kij loop
 
 
 
 
-
-
-  ////////// Accumulation /////////
+  $display("############ Output Verification Start #############"); 
   out_file = $fopen("golden/out.txt", "r");  
-
   // Following three lines are to remove the first three comment lines of the file
   out_scan_file = $fscanf(out_file,"%s", answer); 
   out_scan_file = $fscanf(out_file,"%s", answer); 
   out_scan_file = $fscanf(out_file,"%s", answer); 
-
   error = 0;
-
-
-
-  $display("############ Verification Start during accumulation #############"); 
-
   
 
   #0.5 clk = 1'b0; readout_start = 1'b1;
-  #0.5 clk = 1'b1; 
+  #0.5 clk = 1'b1; // Clock Pos Edge1: core.v notified readout_start
 
-  for (i=0; i<len_onij+1; i=i+1) begin 
-    #0.5 clk = 1'b0; readout_start = 1'b0;
-    
-    if(i>0) begin
-      out_scan_file = $fscanf(out_file,"%128b", answer); // reading from out file to answer
-      if (readout == answer)
-        $display("%2d-th output featuremap Data matched! :D", i); 
-      else begin
-        $display("%2d-th output featuremap Data ERROR!!", i); 
-        $display("sfpout: %128b", readout);
-        $display("answer: %128b", answer);
-        error = 1;
-      end
+  #0.5 clk = 1'b0; readout_start = 1'b0;
+  #0.5 clk = 1'b1; // Clock Pos Edge2: readout port starts output
+
+  for (i=0; i<len_onij; i=i+1) begin 
+    #0.5 clk = 1'b0; // Clock Neg Edge
+
+    out_scan_file = $fscanf(out_file,"%128b", answer); // reading from out file to answer
+    if (readout == answer)
+      $display("%2d-th output featuremap Data matched! :D", i); 
+    else begin
+      $display("%2d-th output featuremap Data ERROR!!", i); 
+      $display("sfpout: %128b", readout);
+      $display("answer: %128b", answer);
+      error = 1;
     end
-    #0.5 clk = 1'b0; 
-    #0.5 clk = 1'b1;
-
+    #0.5 clk = 1'b1; // Clock Pos Edge3: readout port output next
   end
-
 
   if (error == 0) begin
   	$display("############ No error detected ##############"); 
   	$display("########### Project Completed !! ############"); 
-
   end
 
-  $fclose(acc_file);
-  //////////////////////////////////
 
+  //////////////////////////////////
   for (t=0; t<10; t=t+1) begin  
     #0.5 clk = 1'b0;  
     #0.5 clk = 1'b1;  
   end
-
   #10 $finish;
 
 end
-
-always @ (posedge clk) begin
-   inst_w_q   <= inst_w; 
-   D_xmem_q   <= D_xmem;
-   CEN_xmem_q <= CEN_xmem;
-   WEN_xmem_q <= WEN_xmem;
-   A_pmem_q   <= A_pmem;
-   CEN_pmem_q <= CEN_pmem;
-   WEN_pmem_q <= WEN_pmem;
-   A_xmem_q   <= A_xmem;
-   acc_q      <= acc;
-
-end
-
 
 endmodule
 
