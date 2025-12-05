19ef13d61a1e2de923853f252b5bee47fc82bf2c
inQuitiVe <ericcm509mh@gmail.com>
2025-12-05 00:14:00 -0800

    merge alpha 1

--- hardware/src/hw_ref/mac_array.v
+++ hardware/src/hw_ref/mac_array.v
@@ -1,4 +1,6 @@
-module mac_array (clk, reset, out_s, in_w, in_n, inst_w, valid, is_os, act_4b_mode);
+// Created by prof. Mingu Kang @VVIP Lab in UCSD ECE department
+// Please do not spread this code without permission 
+module mac_array (clk, reset, out_s, in_w, in_n, inst_w, valid);
 
   parameter bw = 4;
   parameter psum_bw = 16;
@@ -7,71 +9,21 @@ module mac_array (clk, reset, out_s, in_w, in_n, inst_w, valid, is_os, act_4b_mo
 
   input  clk, reset;
   output [psum_bw*col-1:0] out_s;
-  input  [row*bw-1:0] in_w;
-  input  [2:0] inst_w; // WS: {reserved, execute, kernel loading} / OS: {flush psum, execute, psum loading}
+  input  [row*bw-1:0] in_w; // inst[1]:execute, inst[0]: kernel loading
+  input  [1:0] inst_w;
   input  [psum_bw*col-1:0] in_n;
   output [col-1:0] valid;
-  input  is_os; // whether this is in OS mode
-  input  act_4b_mode;
 
   wire [psum_bw*col*row-1:0] out_s_temp;
   wire [col*row-1:0] valid_temp;
   wire [psum_bw*col*(row+1)-1:0] psum_temp;
-  wire [(row+1)*col:0] in_n_zero_temp;
 
   assign psum_temp[psum_bw*col-1:0] = in_n;
-  assign in_n_zero_temp[0] = !(&in_n[psum_bw*1-1:psum_bw*0]);
-  assign in_n_zero_temp[1] = !(&in_n[psum_bw*2-1:psum_bw*1]);
-  assign in_n_zero_temp[2] = !(&in_n[psum_bw*3-1:psum_bw*2]);
-  assign in_n_zero_temp[3] = !(&in_n[psum_bw*4-1:psum_bw*3]);
-  assign in_n_zero_temp[4] = !(&in_n[psum_bw*5-1:psum_bw*4]);
-  assign in_n_zero_temp[5] = !(&in_n[psum_bw*6-1:psum_bw*5]);
-  assign in_n_zero_temp[6] = !(&in_n[psum_bw*7-1:psum_bw*6]);
-  assign in_n_zero_temp[7] = !(&in_n[psum_bw*8-1:psum_bw*7]);
-
-  // assign out_s = out_s_temp[psum_bw*col*row-1:psum_bw*col*(row-1)];
-  
-  genvar j, k;
-  generate
-    for (j=0; j < col; j=j+1) begin : col_merge
-      wire [psum_bw-1:0] col_psum [0:row-1]; // 2D array: psum from all rows in this column (after mask)
-      
-      for (k=0; k < row; k=k+1) begin : row_collect
-        assign col_psum[k] = inst_sr[j][2] ? out_s_temp[psum_bw*(col*k+j+1)-1:psum_bw*(col*k+j)] : 0;
-      end
-      
-      // OR all masked psums from all rows
-      wire [psum_bw-1:0] or_result [0:row-1];
-      genvar m;
-      for (m=0; m < row; m=m+1) begin : or_tree
-        if (m == 0) begin
-          assign or_result[m] = col_psum[m];
-        end else begin
-          assign or_result[m] = or_result[m-1] | col_psum[m];
-        end
-      end
-      
-      // Output: use OR result in OS mode, otherwise use last row output
-      assign out_s[psum_bw*(j+1)-1:psum_bw*j] = is_os ? or_result[row-1] : 
-                                                 out_s_temp[psum_bw*(col*(row-1)+j+1)-1:psum_bw*(col*(row-1)+j)];
-    end
-  endgenerate
-  
-  // WS mode: valid from last row
-  // OS mode: OR valid from all rows (if any row has valid, output is valid)
-  genvar n, p;
-  generate
-    for (n=0; n < col; n=n+1) begin : valid_merge
-      wire [row-1:0] col_valid; // valid from all rows for this column
-      for (p=0; p < row; p=p+1) begin : valid_collect
-        assign col_valid[p] = valid_temp[col*p+n];
-      end
-      assign valid[n] = is_os ? |col_valid : valid_temp[col*(row-1)+n];
-    end
-  endgenerate
+  assign out_s = out_s_temp[psum_bw*col*row-1:psum_bw*col*(row-1)];
+  assign valid = valid_temp[col*row-1:col*(row-1)];
 
   integer idx;
-  reg [2:0] inst_sr [0:col-1];
+  reg [1:0] inst_sr [0:col-1];
   always@(*) begin
     inst_sr[0] = inst_w;
   end
@@ -94,20 +46,13 @@ module mac_array (clk, reset, out_s, in_w, in_n, inst_w, valid, is_os, act_4b_mo
           .clk(clk),
           .reset(reset),
           .out_s(out_s_temp[psum_bw*col*(i+1)-1:psum_bw*col*i]),
-          .out_s_zero(in_n_zero_temp[col*(i+2)-1:col*(i+1)]),
           .in_w(in_w[bw*(i+1)-1:bw*i]),
           .in_n(psum_temp[psum_bw*col*(i+1)-1:psum_bw*col*i]),
-          .in_n_zero(in_n_zero_temp[col*(i+1)-1:col*i]),
           .valid(valid_temp[col*(i+1)-1:col*i]),
-          .inst_w(inst_sr[i]),
-          .is_os(is_os),
-          .act_4b_mode(act_4b_mode)
-        );
+          .inst_w(inst_sr[i]));
         assign psum_temp[psum_bw*col*(i+2)-1:psum_bw*col*(i+1)] = out_s_temp[psum_bw*col*(i+1)-1:psum_bw*col*i];
     end
   endgenerate
 
 
 endmodule
-
-
--- hardware/src/hw_ref/mac_tile.v
+++ hardware/src/hw_ref/mac_tile.v
@@ -1,152 +1,53 @@
-module mac_tile (
-    clk, 
-    reset,
-    in_w, 
-    in_w_zero,
-    in_n, 
-    in_n_zero,
-    inst_w, 
-    inst_e, 
-    act_4b_mode,
-    is_os, // whether this is in OS mode
-    out_s, 
-    out_s_zero,
-    out_e,
-    out_e_zero
-);
+// Created by prof. Mingu Kang @VVIP Lab in UCSD ECE department
+// Please do not spread this code without permission 
+module mac_tile (clk, out_s, in_w, out_e, in_n, inst_w, inst_e, reset);
 
-    parameter bw = 4;
-    parameter psum_bw = 16;
+parameter bw = 4;
+parameter psum_bw = 16;
 
-    input                clk;
-    input                reset;
-    input  [bw-1:0]      in_w; 
-    input                in_w_zero;
-    input  [psum_bw-1:0] in_n;
-    input                in_n_zero;
-    input  [2:0]         inst_w; // WS: {reserved, execute, kernel loading} / OS: {flush psum, execute, psum loading}
-    input                act_4b_mode;
-    input                is_os; // whether this is in OS mode
+output [psum_bw-1:0] out_s;
+input  [bw-1:0] in_w; // inst[1]:execute, inst[0]: kernel loading
+output [bw-1:0] out_e; 
+input  [1:0] inst_w;
+output [1:0] inst_e;
+input  [psum_bw-1:0] in_n;
+input  clk;
+input  reset;
 
-    output [psum_bw-1:0] out_s;
-    output reg           out_s_zero;
-    output [bw-1:0]      out_e; 
-    output reg           out_e_zero;
-    output [2:0]         inst_e;
+reg [1:0] inst_q;
+reg [bw-1:0] a_q;      // activation
+reg [bw-1:0] b_q;      // weight
+reg [psum_bw-1:0] c_q; // psum
+reg load_ready_q;
+wire [psum_bw-1:0] mac_out;
 
-    // logic registers
-    reg [2:0]            inst_q, inst_q_nxt;
-    reg [bw-1:0]         a_q, a_q_nxt;      // activation
-    reg [bw-1:0]         b_q, b_q_nxt;      // weight
-    reg [psum_bw-1:0]    c_q, c_q_nxt;      // psum
-    reg                  load_ready_q, load_ready_q_nxt; // WS: weight preload, OS: psum preload
-    wire [psum_bw-1:0]   mac_out;
-    wire                 is_preload;
-    wire                 clk_x_gating;
-    wire                 clk_w_gating;
-    wire                 clk_w_gating_temp;
-    wire                 clk_psum_gating;
-    wire                 clk_psum_gating_temp;
-    wire                 in_n_zero_b;
-    wire                 in_w_zero_b;  
+assign out_e = a_q;
+assign inst_e = inst_q;
+assign out_s = mac_out;
 
-    // Output assignments
-    assign out_e  = a_q;
-    assign inst_e = inst_q;
-    // OS mode: when inst_q[2] (flush psum), output accumulated psum (c_q)
-    assign out_s  = ~is_os ? mac_out : inst_q[2] ? c_q : in_n; 
-    assign clk_psum_gating = ~is_os ? clk_w_gating_temp: clk_psum_gating_temp;
-    assign clk_w_gating = ~is_os ? clk_psum_gating_temp: clk_x_gating_temp;
-    // WS mode: weight preload, OS mode: psum preload
-    assign is_preload = inst_w[0] & load_ready_q;
-
-    // Gating
-    assign in_n_zero_b = !in_n_zero;
-    assign in_w_zero_b = !in_w_zero;
-    assign clk_x_gating = in_n_zero_b && clk;
-    assign clk_w_gating_temp = in_w_zero_b && clk;
-    assign clk_psum_gating_temp = (in_n_zero_b || in_w_zero_b) && clk;
-
-    mac #(.bw(bw), .psum_bw(psum_bw)) mac_instance (
+mac #(.bw(bw), .psum_bw(psum_bw)) mac_instance (
         .a(a_q), 
         .b(b_q),
         .c(c_q),
-        .act_4b_mode(act_4b_mode),
-        .out(mac_out)
-    ); 
+	.out(mac_out)
+); 
 
-    // Combinational logic
-    always @(*) begin
-        inst_q_nxt[0]       = (load_ready_q == 0) ? inst_w[0] : inst_q[0];
-        inst_q_nxt[1]       = inst_w[1];
-        inst_q_nxt[2]       = inst_w[2]; // last data / flush psum
-
-        load_ready_q_nxt    = is_preload ? 0 : load_ready_q;
-        
-        a_q_nxt             = (inst_w[0] || inst_w[1]) ? in_w : a_q;
-        
-        if (is_os) begin
-            // When flush (inst_w[2]), don't update b to prevent receiving flushed psum
-            b_q_nxt         = (inst_w[2] == 0 && (inst_w[0] || inst_w[1])) ? in_n[bw-1:0] : b_q;
-            // preload psum when inst_w[0] && load_ready_q, save mac output when inst_w[1]
-            // When flush (inst_w[2]), clear psum
-            c_q_nxt         = inst_w[2] ? 0 : (is_preload ? in_n : (inst_w[1] ? mac_out : c_q));
-        end else begin
-            b_q_nxt         = is_preload ? in_w : b_q;
-            c_q_nxt         = in_n;
-        end
-        
+// Synchronous logic
+always @(posedge clk) begin
+    if (reset == 1) begin
+        inst_q        <= 0;
+        load_ready_q  <= 1;
+        a_q           <= 0;
+        b_q           <= 0;
+        c_q           <= 0;
+    end else begin
+        inst_q[0]    <= (load_ready_q == 0) ? inst_w[0] : inst_q[0];
+        inst_q[1]    <= inst_w[1];
+        load_ready_q <= (inst_w[0] == 1 && load_ready_q == 1) ? 0  : load_ready_q;
+        a_q          <= (inst_w[0] || inst_w[1]) ? in_w : a_q;
+        b_q          <= (inst_w[0] == 1 && load_ready_q == 1) ? in_w : b_q;
+        c_q          <= in_n;
     end
-
-
-    // Synchronous logic
-    always @(posedge clk) begin
-        if (reset) begin
-            inst_q        <= 0;
-            load_ready_q  <= 1;
-            // a_q           <= 0;
-            // b_q           <= 0;
-            // c_q           <= 0;
-            out_s_zero    <= 0;
-            out_e_zero    <= 0;
-        end else begin
-            inst_q       <= inst_q_nxt;
-            load_ready_q <= load_ready_q_nxt;
-            // a_q          <= a_q_nxt;
-            // b_q          <= b_q_nxt;
-            // c_q          <= c_q_nxt;
-            out_s_zero   <= in_n_zero;
-            out_e_zero   <= in_w_zero;
-        end
-    end
-
-    always @(posedge clk_x_gating || reset) begin
-        if (reset) begin
-            a_q <= 0;
-        end
-        else begin
-            a_q <= a_q_nxt;
-        end
-    end
-
-    always @(posedge clk_w_gating || reset) begin
-        if (reset) begin
-            b_q <= 0;
-        end
-        else begin
-            b_q <= b_q_nxt;
-        end
-    end
-
-    always @(posedge clk_psum_gating || reset) begin
-        if (reset) begin
-            c_q <= 0;
-        end
-        else begin
-            c_q <= c_q_nxt;
-        end
-    end
+end
 
 endmodule
-
-
